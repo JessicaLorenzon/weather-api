ee952ed7cd687ca77c14c22ed76bde400eae9467
JessicaLorenzon <jelorenzon@gmail.com>
2025-09-23 12:14:57 -0300

    add redis cache

--- src/main/java/com/lorenzon/weather_api/service/WeatherService.java
+++ src/main/java/com/lorenzon/weather_api/service/WeatherService.java
@@ -5,6 +5,7 @@ import com.lorenzon.weather_api.exception.NotFoundException;
 import com.lorenzon.weather_api.exception.TooManyRequestsException;
 import com.lorenzon.weather_api.exception.UnauthorizedException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -19,6 +20,7 @@ public class WeatherService {
     @Value("${WEATHER_API_KEY}")
     private String apiKey;
 
+    @Cacheable(value = "weatherCache", key = "#location")
     public String getWeatherApi(String location) throws Exception {
         String url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
                 .concat(location).concat("?key=").concat(apiKey);
